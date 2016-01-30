ca27475f662c6f5bfd8006dafe9d4583e4025d05
Josh Holtz <josh@rokkincat.com>
2016-01-30 13:18:32 -0600

    Oh man, we can do database stuff

--- epoch/app/Sources/main.swift
+++ epoch/app/Sources/main.swift
@@ -3,25 +3,69 @@
 #else
     import Darwin.C
 #endif
+
+import Foundation
+
+import Core
 import HTTP
 import Router
 import Epoch
 import CHTTPParser
 import CLibvenice
 
+import PostgreSQL
+
 // Need this because output
 // is not getting flushed properly
-func debug(string: String) {
-  print(string)
+func debug(items: Any...) {
+  debugPrint(items)
   fflush(stdout)
 }
 
+// let processInfo = NSProcessInfo()
+let processInfo = NSProcessInfo.processInfo()
+let env = processInfo.environment
+debug("Env: \(env)")
+
+let info = Connection.Info(host: "epochdb", database: "postgres", port: 5432, user: "postgres", password: nil)
+let conn = Connection(info)
+
+do {
+    try conn.open()
+
+    try conn.execute(
+      "CREATE TABLE  IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT NOT NULL)"
+    )
+
+    let createUsers:[String] = [
+      "Josh",
+      "Jason",
+      "Nick"
+    ]
+
+    // for userName in createUsers {
+    //   try conn.execute("INSERT INTO USERS (name) VALUES($1)", parameters: userName)
+    // }
+
+} catch {
+    // debug(error)
+}
+debug("Conn status: \(conn.status)")
+
 let router = Router { router in
-    router.get("/") { request in
-        // do something based on the Request
-        debug("Yeeaaahhhhhhh")
-        return Response(status: .OK, json: ["Hello": "World"])
-    }
+  router.get("/") { request in
+
+    let results = try conn.execute("SELECT * FROM users")
+    // debug(results)
+    let json: [Any] = results.flatMap { result -> [String: Any]? in
+        guard let id = result["id"]?.string, name = result["name"]?.string else {
+          return nil
+        }
+        return ["id": id, "name": name]
+      }
+
+    return Response(status: .OK, json: Core.JSON.from(json))
+  }
 }
 
 debug("Starting websever")
